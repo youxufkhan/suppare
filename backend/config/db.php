e3682365ffa6bb01f775e7bd873ffa3870c9eaec
Yousuf Khan <m.youxuf@gmail.com>
2018-03-12 18:34:39 +0500

    htaccess/ cors issue fixed

--- backend/config/db.php
+++ backend/config/db.php
@@ -3,8 +3,8 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=supparedb',
-    'username' => 'phpmyadmin',
-    'password' => '123',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { environment } from '../../environments/environment';
-
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/operator/map';
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html'
@@ -13,9 +14,13 @@ export class LayoutComponent implements OnInit {
     }
 
     ngOnInit(){
-
+      this.loadData();
     }
 
-    
+    loadData(){
+      this.http.get('http://localhost/suppare/backend/web/index.php?r=category').map((response)=>{
+        console.log(response) 
+      }).subscribe();
+    }
 
 } 
